7c9c65409ff95fa3e1ab41bc0274a95f541b77ed
Yogesh-Vaidhya <2018eeb1277@iitrpr.ac.in>
2021-01-07 04:34:55 +0530

    Inheritance types

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// Types of Inheritance
+/*
+1. Single Inheritance
+2. Multiple Inheritance
+3. Multi-level inheritance
+4. Hybrid Inheritance
+5. Hierarchical Inheritance
+*/
 class A{
     public:
     void functA(){
